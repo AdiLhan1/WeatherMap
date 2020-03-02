b73c0232612f77d59a5a11a2bb54458a614ab790
AdiLhan1 <kelsinbekovadilhan2018@gmail.com>
2020-03-02 20:56:51 +0600

    add MapFragment, and add lib map

--- app/src/main/java/com/example/weathermap/MainActivity.kt
+++ app/src/main/java/com/example/weathermap/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.weathermap
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,22 +14,24 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupNavigationView() {
-        navigation_view.setOnNavigationItemReselectedListener {
+        navigation_view.setOnNavigationItemSelectedListener {
             when (it.itemId) {
                 R.id.n_map -> {
                     setSelectFragment()
+                    true
                 }
                 R.id.n_city_search -> {
-
+                    true
                 }
                 R.id.n_profile -> {
-
+                    true
                 }
+                else -> false
             }
         }
     }
 
-    private fun setSelectFragment() {
-        supportFragmentManager.beginTransaction().add(R.id.main_fragment,it)
+    private fun setSelectFragment(fr: Fragment? = null) {
+        fr?.let { supportFragmentManager.beginTransaction().add(R.id.main_fragment, it).commit() }
     }
 }

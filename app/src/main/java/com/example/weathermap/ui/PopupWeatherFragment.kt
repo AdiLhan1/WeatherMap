7b5725b9b3ea0c6dfedca2ffd43fa46b58ab04b1
AdiLhan1 <kelsinbekovadilhan2018@gmail.com>
2020-03-05 19:52:25 +0600

    - Сделал закрытие popup Weather Fragment - Убрал toolbar и title - Изменил цвет status bar

--- app/src/main/java/com/example/weathermap/ui/PopupWeatherFragment.kt
+++ app/src/main/java/com/example/weathermap/ui/PopupWeatherFragment.kt
@@ -1,15 +1,11 @@
 package com.example.weathermap.ui
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentTransaction
 import com.example.weathermap.R
 import kotlinx.android.synthetic.main.fragment_popup_weather.*
 
@@ -17,26 +13,22 @@ import kotlinx.android.synthetic.main.fragment_popup_weather.*
  * A simple [Fragment] subclass.
  */
 class PopupWeatherFragment : Fragment() {
-    lateinit var img: ImageView
-    lateinit var context: AppCompatActivity
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        ic_close.setOnClickListener {
+            val fm = activity?.supportFragmentManager
+            val transaction = fm?.beginTransaction()
+            transaction?.remove(this)
+            transaction?.commit()
+            Toast.makeText(context, "Closed", Toast.LENGTH_LONG).show()
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view: View? = inflater.inflate(R.layout.fragment_popup_weather, container, false)
-        ic_close?.setOnClickListener {
-            Log.e("ololo", "hello")
-            Toast.makeText(context, "Hello", Toast.LENGTH_LONG).show()
-//            val fm = context.supportFragmentManager
-//            val fragmentTransaction: FragmentTransaction
-//            val fragment = PopupWeatherFragment()
-//            fragmentTransaction = fm.beginTransaction()
-//            fragmentTransaction.hide(fragment)
-//                .addToBackStack(null)
-//                .set
-//            fragmentTransaction.commit()
-        }
         return view
     }
 
